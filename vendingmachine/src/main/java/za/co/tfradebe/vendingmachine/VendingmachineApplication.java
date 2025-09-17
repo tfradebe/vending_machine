fd2cc2008ae6dc89968c9286c7d147b6df6e745e
Tefo <tfradebe@gmail.com>
2025-09-17 23:04:41 +0200

    Init commit

---
+++ vendingmachine/src/main/java/za/co/tfradebe/vendingmachine/VendingmachineApplication.java
@@ -0,0 +1,13 @@
+package za.co.tfradebe.vendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingmachineApplication.class, args);
+	}
+
+}
---
+++ vendingmachine/src/test/java/za/co/tfradebe/vendingmachine/VendingmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.tfradebe.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
