1bdaeac02f878edc55207a73d23a92d0229dac95
Tefo <tfradebe@gmail.com>
2025-09-20 12:25:59 +0200

    Inventory Development

---
+++ payment/src/main/java/za/co/tfradebe/payment/PaymentApplication.java
@@ -0,0 +1,13 @@
+package za.co.tfradebe.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentApplication.class, args);
+	}
+
+}
