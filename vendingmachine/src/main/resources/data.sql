3bd07f5e64c993455c3a2291179d7e2d4bd027d6
Tefo <tfradebe@gmail.com>
2025-09-23 21:59:58 +0200

    Payment API

--- vendingmachine/src/main/resources/data.sql
+++ vendingmachine/src/main/resources/data.sql
@@ -1,16 +1,16 @@
-INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('SIMBA','AA',5.0,10);
-INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('NICKNACK','AB',10.0,10);
-INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('FANTA','BA',15.0,10);
-INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('COCACOLA','BB',15.0,10);
-INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('REDBULL','CA',25.0,10);
+INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('SIMBA','AA',5,10);
+INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('NICKNACK','AB',10,10);
+INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('FANTA','BA',15,10);
+INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('COCACOLA','BB',15,10);
+INSERT INTO PRODUCT (name,location,price,quantity) VALUES ('REDBULL','CA',25,10);
 
 // Load money
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('ONE',10);
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TWO',10);
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('FIVE',10);
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TEN',10);
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TWENTY',10);
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('FIFTY',10);
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('HUNDRED',10);
-INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TWO_HUNDRED',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('ONE_RAND',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TWO_RAND',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('FIVE_RAND',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TEN_RAND',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TWENTY_RAND',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('FIFTY_RAND',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('ONE_HUNDRED_RAND',10);
+INSERT INTO LOADED_MONEY (amount,quantity) VALUES ('TWO_HUNDRED_RAND',10);
 
